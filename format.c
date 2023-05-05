d19ab2bf769f7f9d16a3332bc5bbffce43c3464f
enso02 <ensar.baran@stud.thga.de>
2023-05-05 21:20:39 +0200

    format specifier code hinzugefügt

---
+++ format.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() 
+{
+  int ganzz = 70;
+  float gleitkommaz = 2.6235;
+  char character = 'e';
+  char str[] = "Hello World";
+  
+  printf("Ganzzahl: %i\n", ganzz);
+  printf("Gleitkommazahl: %f\n", gleitkommaz);
+  printf("Character: %c\n", character);
+  printf("String: %s\n", str);
+  
+  return 0;
+}
+
